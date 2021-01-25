49ee5ca149b3412963682c903bc8665ef71d563b
PrachiHejib <p.hejib@uea.ac.uk>
2021-01-25 10:02:32 +0000

    set up generic question and answer structure for Numeracy pages

--- numeracy_survey/pages.py
+++ numeracy_survey/pages.py
@@ -4,30 +4,37 @@ from .models import Constants
 
 
 class Page1(Page):
+    template_name = "numeracy_survey/GeneralNumeracy.html"
     form_model = 'player'
     form_fields = ['answer1']
 
 class Page2(Page):
+    template_name = "numeracy_survey/GeneralNumeracy.html"
     form_model = 'player'
     form_fields = ['answer2']
 
 class Page3(Page):
+    template_name = "numeracy_survey/GeneralNumeracy.html"
     form_model = 'player'
     form_fields = ['answer3']
 
 class Page4(Page):
+    template_name = "numeracy_survey/GeneralNumeracy.html"
     form_model = 'player'
     form_fields = ['answer4']
 
 class Page5(Page):
+    template_name = "numeracy_survey/GeneralNumeracy.html"
     form_model = 'player'
     form_fields = ['answer5']
 
 class Page6(Page):
+    template_name = "numeracy_survey/GeneralNumeracy.html"
     form_model = 'player'
     form_fields = ['answer6']
 
 class Page7(Page):
+    template_name = "numeracy_survey/GeneralNumeracy.html"
     form_model = 'player'
     form_fields = ['answer7']
 
