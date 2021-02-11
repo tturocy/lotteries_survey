c2a691a0983e33e4e563d9a7dbb3a3ec08ebe0c6
Theodore Turocy <ted.turocy@gmail.com>
2021-02-11 11:02:06 +0000

    Migrate menu sequence to participant, not the group.

--- decisions/models.py
+++ decisions/models.py
@@ -32,6 +32,11 @@ class Subsession(BaseSubsession):
                             index_col='roll')
                 .transpose()
             )
+        for (menu_seq, player) in enumerate(self.get_players()):
+            player.participant.vars['menu_seq'] = menu_seq
+
+    def get_menu_number(self, round_number, group_number):
+        return round_number
 
 
 class MenuItem:
@@ -47,17 +52,7 @@ class MenuItem:
 
 
 class Group(BaseGroup):
-    def get_menu(self):
-        return [
-            MenuItem(
-                f"decisions/lottery_p{self.round_number}.jpg",
-                self.session.vars['lotteries'].loc[f"p{self.round_number}"]
-            ),
-            MenuItem(
-                f"decisions/lottery_q{self.round_number}.jpg",
-                self.session.vars['lotteries'].loc[f"q{self.round_number}"]
-            )
-        ]
+    pass
 
 
 class PlayerChoice:
@@ -75,9 +70,22 @@ class Player(BasePlayer):
     # Dice rolls are determined at subsession initialisation
     roll = models.IntegerField()
 
+    def get_menu(self):
+        menu_number = self.round_number
+        return [
+            MenuItem(
+                f"decisions/lottery_p{menu_number}.jpg",
+                self.session.vars['lotteries'].loc[f"p{menu_number}"]
+            ),
+            MenuItem(
+                f"decisions/lottery_q{menu_number}.jpg",
+                self.session.vars['lotteries'].loc[f"q{menu_number}"]
+            )
+        ]
+
     def process_decision(self):
         self.roll = random.randint(1, 100)
-        item = self.group.get_menu()[self.lotterychoice]
+        item = self.get_menu()[self.lotterychoice]
         if 'choices' not in self.participant.vars:
             self.participant.vars['choices'] = {}
         self.participant.vars['choices'][self.round_number] = (
