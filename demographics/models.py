6b29a428ca26e5023624ce855f99c5d44d04dc1b
PrachiHejib <p.hejib@uea.ac.uk>
2021-02-24 21:57:58 +0000

    Standardise Demographics pages

--- demographics/models.py
+++ demographics/models.py
@@ -33,36 +33,44 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     gender = models.IntegerField(
+        label="1.	What gender do you identify as?",
         choices = [
         [1, 'Male'],
         [2, 'Female'],
         [3, 'Other'],
         ],
-        widget=widgets.RadioSelect
+        widget=widgets.RadioSelect,
+        blank=True,
     )
 
     genderother = models.StringField(
-        label="",
+        label="If other please specify:",
         blank = True,
     )
 
     age = models.IntegerField(
-        label="",
+        label="2.	How old are you?",
+        blank=True,
     )
 
     countryborn = models.StringField(
-        label="",
+        label="3.	Which country (or countries) were you a citizen of when you were born?",
+        blank=True,
     )
 
     countrynow = models.StringField(
-        label="",
+        label="4.	In which country is your current permanent residence? "
+              "(If you are in the UK on a student visa, this is where “home” is.)",
+        blank=True,
     )
 
     department = models.StringField(
-        label="",
+        label="5.	In which School are you currently enrolled? (For example, BIO, ECO, EDU…)",
+        blank=True,
     )
 
     degree = models.IntegerField(
+        label="6.	What type of degree course are you currently enrolled on?",
         choices = [
             [1, 'INTO'],
             [2, 'Bachelor'],
@@ -72,14 +80,25 @@ class Player(BasePlayer):
             [6, 'Staff'],
             [7, 'Other'],
             ],
-        widget=widgets.RadioSelect
+        widget=widgets.RadioSelect,
+        blank=True,
     )
 
     degreeother = models.StringField(
-        label="",
+        label="If other please specify:",
+        blank=True,
     )
 
 
     timeuea = models.IntegerField(
-        label="",
+        label="7.	How long have you been at UEA?",
+        choices=[
+            [1, 'less than 1 year'],
+            [2, '1-2 years'],
+            [3, '2-3 years'],
+            [4, '3-4 years'],
+            [5, ' more than 4 years'],
+            ],
+            widget = widgets.RadioSelect,
+        blank=True,
     )
--- demographics/pages.py
+++ demographics/pages.py
@@ -2,23 +2,61 @@ from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
-class Demographics1(Page):
-    form_model = 'player'
-    form_fields = ['gender',
-                   'genderother',
-                   'age',
-                   'countryborn',
-                   'countrynow',
-                   'department',
-                   'degree',
-                   'degreeother',
-                   'timeuea',
-                   ]
-
-
-class Demographics2(Page):
+class DemographicsIntro(Page):
     pass
 
-page_sequence = [Demographics1,
-                 Demographics2,
+class DemographicsPage1(Page):
+    template_name = "Demographics/GeneralDemographics.html"
+    form_model = 'player'
+    form_fields = ['gender']
+
+class DemographicsPage1(Page):
+    template_name = "Demographics/GeneralDemographics.html"
+    form_model = 'player'
+    form_fields = ['gender',
+                   'genderother'
+                   ]
+
+class DemographicsPage2(Page):
+    template_name = "Demographics/GeneralDemographics.html"
+    form_model = 'player'
+    form_fields = ['age']
+
+class DemographicsPage3(Page):
+    template_name = "Demographics/GeneralDemographics.html"
+    form_model = 'player'
+    form_fields = ['countryborn']
+
+class DemographicsPage4(Page):
+    template_name = "Demographics/GeneralDemographics.html"
+    form_model = 'player'
+    form_fields = ['countrynow']
+
+class DemographicsPage5(Page):
+    template_name = "Demographics/GeneralDemographics.html"
+    form_model = 'player'
+    form_fields = ['department']
+
+class DemographicsPage6(Page):
+    template_name = "Demographics/GeneralDemographics.html"
+    form_model = 'player'
+    form_fields = ['degree',
+                   'degreeother',
+                   ]
+
+class DemographicsPage7(Page):
+    template_name = "Demographics/GeneralDemographics.html"
+    form_model = 'player'
+    form_fields = ['timeuea']
+
+
+
+page_sequence = [DemographicsIntro,
+                 DemographicsPage1,
+                 DemographicsPage2,
+                 DemographicsPage3,
+                 DemographicsPage4,
+                 DemographicsPage5,
+                 DemographicsPage6,
+                 DemographicsPage7,
                  ]
